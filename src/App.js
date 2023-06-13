3df7465545f5519f41f84a4c5ce529b09ffad52d
Arttutu <arthurtt08@gmail.com>
2023-06-13 19:45:29 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { GlobalStyle } from "./Components/GlobalStyles";
+import Header from "./Components/Header";
+
+
+function App() {
+  return ( 
+    <>
+      <GlobalStyle/>
+      <Header />
+    </>
+  )
+}
+
+export default App;
---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      Fintech APP
+      <nav>
+        <ul>
+            <li>Login</li>
+            <li>Cadastre</li>
+            <li>Sobre Nós</li>
+            <li>Fale Conosco</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
