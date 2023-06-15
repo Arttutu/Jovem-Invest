c72dfc4d4a2731ba7d1d170dec12a4a6c15bc3c7
Arttutu <arthurtt08@gmail.com>
2023-06-14 21:27:12 -0300

    adicionando responsividade

--- src/Components/Formulario/index.jsx
+++ src/Components/Formulario/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { styled } from 'styled-components'
 import { Font } from '../UI/variaveis'
-import { BotaoPrincipal, Titulo } from '../UI'
+import { BotaoPrincipal } from '../UI'
 const BoxForm = styled.div`
     display: flex;
     justify-content: center;
     width: 50%;  
+    @media (max-width:768px){
+        width: 100%;
+    
+    }
 `
 const FormStyle = styled.form`
     display: flex;
@@ -17,9 +21,19 @@ const LabelStyle = styled.label`
     font-size: 1.5em;
 `
 const InputStyle = styled.input`
+   font-size: 1.3em;
+   font-style: ${Font};
    width: 400px;
+   margin: 0 auto;
+   height: auto;
    padding: 10px;
+   box-sizing: border-box;
+   @media (max-width:768px){
+        width: 350px;
+        
+    }
 `
+
 export default function Formulario() {
   return (
    <>
